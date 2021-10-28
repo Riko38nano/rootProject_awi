0305d4ad07fcf2393f08914b7b9151a2b9676c84
rikonano <galoisnicolas@gmail.com>
2021-10-28 11:30:50 +0200

    del backup localisation

--- backup/repositories/LocationRepository.java
+++
@@ -1,7 +0,0 @@
-package project.awi.localisation.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import project.awi.localisation.models.Location;
-
-public interface LocationRepository extends JpaRepository<Location, Long> {
-}
